6a4ecc57026ac1586735e0eb0b0f67d650841900
Peace Ogunleye <009955.ot@gmail.com>
2026-09-07 18:03:33 +0100

    Configure backend for Vercel

--- server/server.js
+++ server/server.js
@@ -55,8 +55,4 @@ app.post("/api/contact", async (req, res) => {
 	}
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+export default app;
